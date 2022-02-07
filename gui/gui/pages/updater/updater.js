8b14664ad5e35a6e2977b88ec39875e72945b1fd
Sklavos IT <kalo-pc@windowslive.com>
2022-02-07 13:53:09 +0200

    Delete gui/gui directory

--- gui/gui/pages/updater/updater.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer } = require('electron');
-const version = document.getElementById('version');
-    
-ipcRenderer.send('app_version');
-ipcRenderer.on('app_version', (event, arg) => {
-    ipcRenderer.removeAllListeners('app_version');
-    version.innerText = 'Version ' + arg.version;
-});
